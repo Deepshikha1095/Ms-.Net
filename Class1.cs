68122ebad2a82cd7c0a00948e2f83d0ed4cf096d
Deepshikha1095 <deepshikha.kurre@gmail.com>
2024-11-03 09:21:23 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Operations
+    {
+        public List<Employee> GetAllEmployees()
+        {
+            DacWondersEntities1 DacWondersEntities = new DacWondersEntities1();
+            List<Employee> lst = (from emp in DacWondersEntities.Employees select emp).ToList();
+            DacWondersEntities.Dispose();
+            return lst;
+        }
+        public void AddEmployee(Employee emp)
+        {
+            using (DacWondersEntities1 DacWondersEntities = new DacWondersEntities1())
+            {
+                DacWondersEntities.Employees.Add(emp);
+                DacWondersEntities.SaveChanges();
+            }
+        }
+        public void UpdateEmployee(Employee empToBeModified)
+        {
+            using (DacWondersEntities1 dacWondersEntities = new DacWondersEntities1())
+            {
+                var empSelected = (from emp in dacWondersEntities.Employees
+                                   where emp.eid == empToBeModified.eid
+                                   select emp).First();
+                empSelected.ename = empToBeModified.ename;
+                empSelected.dept = empToBeModified.dept;
+                dacWondersEntities.SaveChanges();
+            }
+        }
+        public void DeleteEmployee(int id)
+        {
+            using (DacWondersEntities1 dacWondersEntities = new DacWondersEntities1())
+            {
+                var empSelected = (from emp in dacWondersEntities.Employees
+                                   where emp.eid == id
+                                   select emp).First();
+                dacWondersEntities.Employees.Remove(empSelected);
+                dacWondersEntities.SaveChanges();
+            }
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using Library;
+
+namespace MVC_Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        Operations operations = new Operations();
+        public ActionResult Index()
+        {
+            return View(operations.GetAllEmployees());
+        }
+
+        [HttpGet]
+        public ActionResult AddEmp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmp(Employee e)
+        {
+            operations.AddEmployee(e);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UpdateEmployee(int id)
+        {
+            var emp = (from em in operations.GetAllEmployees() where em.eid == id select em).First();
+            return View(emp);
+
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEmployee(Employee emp)
+        {
+            operations.UpdateEmployee(emp);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteEmployee(int id)
+        {
+            operations.DeleteEmployee(id);
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
